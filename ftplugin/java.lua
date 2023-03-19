3f21256b48988b27f5464395e35edc779ecf50a3
Borislav Semerdzhiev <borislavsemerdzhiev.02@gmail.com>
2023-03-19 19:42:48 +0100

    Initial commit

---
+++ ftplugin/java.lua
@@ -0,0 +1,5 @@
+local config = {
+	cmd = { "/Users/str1ct0wn3r/Documents/java-ls/bin/jdtls" },
+	root_dir = vim.fs.dirname(vim.fs.find({ "gradlew", ".git", "mvnw" }, { upward = true })[1]),
+}
+require("jdtls").start_or_attach(config)
